eb247eb15a267ace00e0424ba888fddeb4228d7a
jeonjonghyeok <tongher1685@gmail.com>
2021-09-23 05:05:09 +0900

    feat: kafka topic 변경

--- src/main/java/com/bithumb/consumer/service/CoinServiceImpl.java
+++ src/main/java/com/bithumb/consumer/service/CoinServiceImpl.java
@@ -14,10 +14,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CoinServiceImpl implements CoinService{
     private final MessageSender messageSender;
-    @KafkaListener(topics = "kafka-spring-producer-coin-test3",containerFactory = "coinKafkaListenerFactory")
+    @KafkaListener(topics = "kafka-spring-producer-coin-test4",containerFactory = "coinKafkaListenerFactory")
     public void listenHeaders(
             @Payload Quote quote,
             @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) throws JsonProcessingException {
+        System.out.println("listen msg");
         System.out.println("Received Message: \n"+ quote + "\n from partition: "+ partition);
         messageSender.sendMessage(quote);
     }
