c797815eed6003d31fc8d92fdd16f41762450982
jeonjonghyeok <tongher1685@gmail.com>
2021-09-30 19:58:27 +0900

    feat: docker 파일 jar 업데이트 포트변경

--- src/main/java/com/bithumb/consumer/service/KafkaListenerServiceImpl.java
+++ src/main/java/com/bithumb/consumer/service/KafkaListenerServiceImpl.java
@@ -1,25 +1,25 @@
-package com.bithumb.consumer.service;
-
-import com.bithumb.websocket.config.MessageSender;
-import com.bithumb.websocket.domain.Quote;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class KafkaListenerServiceImpl implements KafkaListenerService {
-    private final MessageSender messageSender;
-    @KafkaListener(topics = "kafka-spring-producer-coin-test5",containerFactory = "coinKafkaListenerFactory")
-    public void listenHeaders(
-            @Payload Quote quote,
-            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) throws JsonProcessingException {
-        System.out.println("listen msg");
-        System.out.println("Received Message: \n"+ quote + "\n from partition: "+ partition);
-        messageSender.sendMessage(quote);
-    }
-}
+//package com.bithumb.consumer.service;
+//
+//import com.bithumb.websocket.config.MessageSender;
+//import com.bithumb.websocket.domain.Quote;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.support.KafkaHeaders;
+//import org.springframework.messaging.handler.annotation.Header;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class KafkaListenerServiceImpl implements KafkaListenerService {
+//    private final MessageSender messageSender;
+//    @KafkaListener(topics = "kafka-spring-producer-coin-test5",containerFactory = "coinKafkaListenerFactory")
+//    public void listenHeaders(
+//            @Payload Quote quote,
+//            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) throws JsonProcessingException {
+//        System.out.println("listen msg");
+//        System.out.println("Received Message: \n"+ quote + "\n from partition: "+ partition);
+//        messageSender.sendMessage(quote);
+//    }
+//}
