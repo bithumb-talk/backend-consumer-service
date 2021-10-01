bcecb277f0dbedcdd919818091eae46f4e0b459d
jeonjonghyeok <tongher1685@gmail.com>
2021-10-01 15:07:43 +0900

    feat: client host 추가

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,7 +13,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/subscribe');
+    var socket = new SockJS('http://localhost:8080/subscribe');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
