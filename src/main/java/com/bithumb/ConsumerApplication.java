cf66701dce26b7eff9c90e4630f7ac9f0172c80c
jeonjonghyeok <tongher1685@gmail.com>
2021-10-01 22:33:58 +0900

    feat: 유레카 적용

--- src/main/java/com/bithumb/ConsumerApplication.java
+++ src/main/java/com/bithumb/ConsumerApplication.java
@@ -2,8 +2,11 @@ package com.bithumb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication(scanBasePackages={"com.bithumb.consumer","com.bithumb.websocket","com.bithumb.quoteinit","com.bithumb.coin","com.bithumb.candlestick","com.bithumb.common","com.bithumb.changerate"})
+@EnableEurekaClient
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
